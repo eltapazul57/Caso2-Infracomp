918b9ab20f2486f788539568dc7dff2012d1acd1
Manuel Gómez <m.gomez49@uniandes.edu.co>
2024-10-07 19:05:24 -0500

    opción esconder mensaje falta terminar

--- App.java
+++ App.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class App {    
@@ -33,7 +35,51 @@ public class App {
         int pageSize= scanner.nextInt();
         System.out.print("Ingrese el nombre del archivo que guarda la imagen: ");
         String fileName = scanner.next();
-        
+        //TODO terminar de implementar
     }
 
+    public void opcionCalcularFallas(Scanner scanner) {
+        //TODO
+    }
+    public void opcionEsconderMensaje(Scanner scanner) {
+        //TODO
+        /* 
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        try {
+            System.out.println("Nombre del archivo con la imagen a procesar: ");
+            ruta = br.readLine();
+            // Cargar la imagen desde la ruta proporcionada
+            imagen = new Imagen(ruta);
+            System.out.println("Nombre del archivo con el mensaje a esconder: ");
+            String ruta2 = br.readLine();
+            // Leer el archivo de texto que contiene el mensaje
+            int longitud = leerArchivoTexto(ruta2);
+            // Esconder el mensaje en la imagen
+            imagen.esconder(mensaje, longitud);
+            // Escribir la imagen modificada con el mensaje escondido
+            imagen.escribirImagen("salida" + ruta);
+            // El bitmap de salida debería poder abrirse en un editor de imágenes sin percibir cambios
+            br.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        */
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+        try {
+            System.out.println("Nombre del archivo con la imagen a procesar: ");
+            String ruta = br.readLine();
+            Imagen imagen = new Imagen(ruta);
+            System.out.println("Nombre del archivo con el mensaje a esconder: ");
+            String ruta2 = br.readLine();
+            //int longitud = leerArchivoTexto(ruta2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void opcionRecuperarMensaje(Scanner scanner) {
+        //TODO
+    }
 }
