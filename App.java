1e984e3aec6d26f19fed3a9ef6275bd1e0c5bb0a
Manuel Gómez <m.gomez49@uniandes.edu.co>
2024-10-13 14:55:45 -0500

    esconderMensaje funcionando

--- App.java
+++ App.java
@@ -1,8 +1,14 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
 import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Paths; 
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class App {    
     public static void main(String[] args) {
@@ -27,6 +33,9 @@ public class App {
                 case 1:
                     opcionGenerarReferencias(scanner);
                     break;
+                case 2:
+                    opcionCalcularFallas(scanner);
+                    break;
                 case 3:
                     opcionEsconderMensaje(scanner);
                     break;
@@ -49,46 +58,41 @@ public class App {
     public void opcionCalcularFallas(Scanner scanner) {
         //TODO
     }
+
+
     public void opcionEsconderMensaje(Scanner scanner) {
-        //TODO
-        /* 
+        //TODO PROBAR CON MÁS CASOS
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
+
         try {
-            System.out.println("Nombre del archivo con la imagen a procesar: ");
-            ruta = br.readLine();
-            // Cargar la imagen desde la ruta proporcionada
-            imagen = new Imagen(ruta);
-            System.out.println("Nombre del archivo con el mensaje a esconder: ");
-            String ruta2 = br.readLine();
-            // Leer el archivo de texto que contiene el mensaje
-            int longitud = leerArchivoTexto(ruta2);
-            // Esconder el mensaje en la imagen
-            imagen.esconder(mensaje, longitud);
-            // Escribir la imagen modificada con el mensaje escondido
-            imagen.escribirImagen("salida" + ruta);
-            // El bitmap de salida debería poder abrirse en un editor de imágenes sin percibir cambios
-            br.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        */
-        InputStreamReader isr = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(isr);
-        try {
+            //Lectura de IMAGEN
             System.out.println("Nombre del archivo con la imagen a procesar: ");
             String ruta = br.readLine();
-            Imagen imagen = new Imagen(ruta);
+            Imagen imagen = new Imagen(ruta); //generar la imagen para usarla
+            System.out.println("LONGITUD IMAGEN: "+imagen.leerLongitud());
+            //Lectura de MENSAJE
             System.out.println("Nombre del archivo con el mensaje a esconder: ");
             String ruta2 = br.readLine();
-            //int longitud = leerArchivoTexto(ruta2);
+            String contenido = new String(Files.readAllBytes(Paths.get(ruta2)));
+            System.out.println("LONGITUD MENSAJE: "+contenido.length());
+            char[] arregloCaracteres = contenido.toCharArray();
+            System.out.println("LONGITUD ARREGLO CARACTERES: " + arregloCaracteres.length);
+            //Esconder mensaje
+            imagen.esconder(arregloCaracteres, arregloCaracteres.length);
+            imagen.escribirImagen(ruta);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-    }
+        
 
+        
+        
+    }
+    
     public void opcionRecuperarMensaje(Scanner scanner) {
-        //PROBAR CON MÁS CASOS
+        //TODO PROBAR CON MÁS CASOS
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         try {
@@ -99,6 +103,7 @@ public class App {
             // Leer la imagen y el mensaje escondido
             Imagen imagen = new Imagen(ruta);
             int longitud = imagen.leerLongitud();
+            System.out.println("LONGITUD IMAGEN: " + longitud);
             char[] mensaje = new char[longitud];
             // Recuperar el mensaje de la imagen
             imagen.recuperar(mensaje, longitud);
